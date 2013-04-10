cca034ae99cdf36711496d7856521726c3b3dc78
unknown <bcharas@andrew.cmu.edu>
2013-04-09 21:43:30 -0400

    added google feed api demo url access

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.get("/",function(request,response) {
   response.sendfile("static/index.html");
 });
 
+app.get("/feed_api_demo",function(request,response) {
+  response.sendfile("static/feed_demo/feed.html");
+});
 //initServer();
 app.listen("3000");
 //console.log("Listening at 80");
