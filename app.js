b2c417a4f46e2e1439b5db7336194a4496dddef2
Jack Paparian <jackpaparian@gmail.com>
2013-04-16 00:52:23 -0400

    Applied app.use instead of response.sendfile

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var app = express();
-
-var fs = require('fs');
-app.use(express.bodyParser());
-
-app.get("/",function(request,response) {
+n(request,response) {
   response.sendfile("static/index.html");
 });
 
@@ -12,10 +8,18 @@ app.get("/feed_api_demo",function(request,response) {
   response.sendfile("static/feed.html");
 });
 
+var static_dir = express.static(__dirname + "/static");
+
+app.use(static_dir, function(request, response, next){
+  next();
+});
+
+/*
 app.get("/static/:filename", function(request, response) {
   response.sendfile("static/" + request.params.filename);
 });
+*/
 
-//initServer();
 app.listen("8889");
-//console.log("Listening at 80");
+console.log("Listening at 8889");
+
