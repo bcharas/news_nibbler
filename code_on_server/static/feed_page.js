05b3d30c5094a8146984ee32d01240c49ef28c36
unknown <bcharas@andrew.cmu.edu>
2013-04-26 18:44:46 -0400

    Cleaned up code and added user ability to delete feeds

---
+++ code_on_server/static/feed_page.js
@@ -0,0 +1,71 @@
+//handles posting entries of user's feeds to feed page
+
+/*
+//moved globals to init.js
+//moved google.load call to init.js
+//moved default_feeds() to init.js
+//moved parse_all_feeds to parse_feed_data.js
+//moved load_feed_contents to parse_feed_data.js
+//moved load_next_feed to parse_feed_data.js
+//moved init_feeds() to init.js
+//moved google.setOnLoadCallback(init_feeds) to init.js
+//moved publishable_entry_field_data to unused_code.js
+*/
+
+//publishes pre-parsed user feed contents
+//changed name from post_merged_feed
+function post_feeds(){
+	//console.log("posting merged feed");
+	var entries_per_feed = 4;
+	var entry_num = 0;
+	for (var i = 0; i < entries_per_feed; i++){	
+		for (var j = 0; j < window.num_feeds; j++){
+			var saved_feed = window.user_feeds[j];
+			var this_feed = window.active_feeds[j];			
+			if (!(saved_feed.ignore)){
+				var this_entry = this_feed[i];
+				//console.log(this_entry.link);
+				post_feed_entry(this_entry, entry_num);
+				//console.log("posted entry: " + entry_num);
+				entry_num++;
+			}
+			else{
+				console.log("ignored entry: " + entry_num);
+				entry_num++;
+			}
+		}
+	}
+	console.log("feed loaded");
+	//new_subscription_input();
+}
+
+//publishes pre-parsed contents of a given feed entry
+function post_feed_entry(entry, id){
+	
+	var div_class = "listing";
+	var new_div_html = "<div id=" + id + " class='listing'> </div>";
+	var container = $("#feed_box");
+	container.append(new_div_html);
+	var div = $("#" + id);
+	div.html("<div class='headline'>" + entry.title + "</div>");
+	
+	if ((entry.date !== undefined) && (entry.date !== "")){
+		div.append("<br/>" + "<div class='date'>" + entry.publishedDate + "</div>");
+	}
+	div.append("<div class='seperator'></div>")
+	if ((entry.contentSnippet !== undefined) && (entry.contentSnippet !== "")){
+		div.append("<br/>" + "<div class='snippet'>" + entry.contentSnippet + "</div>");
+	}
+	if ((entry.link !== undefined) && (entry.link !== "")){
+		div.append("<br/>" + "<div class='date'>" + entry.link + "</div>");
+	}
+	div.append("<div class='source'>" + "Because you subscribe to: " + entry.feed_src.feed_name + "</div>");
+	div.click(function(){
+             //   localStorage.link = entry.link;
+                closeContainers();
+                $("#article_container").removeClass("hidden");
+				$("#article").attr("src", entry.link);
+      //  window.top.location = './article.html';
+			});
+}
+
--- code_on_server/static/topic_page.js
+++ code_on_server/static/topic_page.js
@@ -1,28 +1,29 @@
-//all of this code was originally in custom_feeds.js
-window.user_feeds = [];
-//default_feed_directory();
+//Handles posting subscriptions to the topic page, and adding new ones via user input
 
-//constructor for  feed object
-function Feed(name, url){
-	this.feed_name = name;
-	this.feed_src = url;
-	this.connection = new google.feeds.Feed(url);
-	window.user_feeds.push(this);
+/*
+//moved to globals to init.js
+//constructor Feed moved to feed.js
+//delete_feed moved to feed.js
+//moved default_feed_directory to init.js
+*/
+
+
+//loops through user feeds and posts a subscription listing for each
+//changed name from publish_feed_directory
+function post_subscriptions(){	
+	var directory = $("#feed_subs");
+	directory.empty();
+	////console.log("user feeds length: " + window.user_feeds.length);
+	for (var i = 0; i < window.user_feeds.length; i++){
+		var feed = window.user_feeds[i];
+		post_subscription_listing(feed, directory);
+	}
+	//console.log("directory complete");
 }
 
-//loads all of the feeds for the default case
-function default_feed_directory(){
-	window.user_feeds = [];
-	//console.log("getting default directory...");
-	var reddit = new Feed("Reddit", "http://www.reddit.com/.rss");	
-	var digg = new Feed("Digg", "http://www.digg.com/rss/index.xml");
-	var cnn = new Feed("CNN", "http://rss.cnn.com/rss/cnn_topstories.rss");
-//	var hacker_news = new Feed("Hacker_News", "https://news.ycombinator.com/rss");
-    var cooking = new Feed("Cooking_with_Friends", "http://www.cookingwithfriendsclub.com/index.php?/rss/blog");
-}
-
-//publishes the topic box for a given feed
-function publish_feed_listing(feed, container){
+//posts the subscription listing for a given feed
+//changed name from publish_feed_listing
+function post_subscription_listing(feed, container){
 	var div = "<div class='topic' ";
 	div += "id='" + feed.feed_name + "'></div>"
 	container.append(div);
@@ -37,16 +38,60 @@ function publish_feed_listing(feed, container){
 	published_div.selected = true;
 	published_div.append(to_publish);
 	feed.ignore = false;
+	
+	
+	var delete_feed_html = "<div id='" + feed.feed_name + "' class='delete_topic_button'> Unsubscribe </div>";
+	published_div.append(delete_feed_html);
+	
+	var delete_this_feed = function(){
+								delete_feed(feed.feed_name);
+							};
+	var delete_button = $("#" + feed.feed_name);
+	delete_button.click(delete_this_feed);
 }
 
-//loops through user feeds and publishes topic entry for each
-function publish_feed_directory(){	
-	var directory = $("#feed_subs");
-	directory.empty();
-	////console.log("user feeds length: " + window.user_feeds.length);
-	for (var i = 0; i < window.user_feeds.length; i++){
-		var feed = window.user_feeds[i];
-		publish_feed_listing(feed, directory);
+//builds the box that allows users to subscribe to new feeds
+//changed name from custom_new_feed_entry
+function new_subscription_input(){
+	var directory = $("#browse");
+	var div_html = "<div class='snippet' ";
+	div_html += "id='custom_feed'><div class= 'headline'>Add another feed:</div> </div>";
+	directory.append(div_html);
+	var new_div = $("#custom_feed");
+	var name_input = "Name <input id='name_input' class='data'></input>";
+	var url_input = "<br/> Url<input id='url_input' class='data'></input>";
+	var subscribe_button_html = "<div id='generator_button'> Subscribe! </div>";
+	new_div.append(name_input);
+	new_div.append(url_input);
+	new_div.append(subscribe_button_html);
+	
+	var subscribe_button = $("#generator_button");
+	subscribe_button.click(subscribe);
+	
+}
+
+//called whenever a user presses subscribe, tries to subscribe to input feed
+//changed name from generate_feed
+function subscribe(){
+	console.log("generating!");
+	var input_name = $("#name_input").val();
+	var input_url = $("#url_input").val();
+	if (input_name == ""){
+		console.log("Feeds must be named before they can be added");
+		return;
 	}
-	//console.log("directory complete");
+	if (input_url == ""){
+		console.log("No feed URL supplied for generator");
+		return;
+	}
+	/*
+	if (!(is_unique_feed(input_name, input_url))){
+		console.log("Requested feed uses occupied name or url");
+		return;
+	}
+	test_potential_feed(input_name, input_url);
+	*/
+	
+	add_new_feed(input_name, input_url);
+	
 }
